fc6ebbe2886545c84d6eeaadbf6f779f085a4d4d
noonkey <noonkey.soft@gmail.com>
2015-09-20 00:22:59 +0300

    Gui change

--- gui.py
+++ gui.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*- 
-
-###########################################################################
-# Python code generated with wxFormBuilder (version Jun 17 2015)
-# http://www.wxformbuilder.org/
-#
-# PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
 import wx
 import wx.xrc
 import wx.html
